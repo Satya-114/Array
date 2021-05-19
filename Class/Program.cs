dde42d51cd7640caf498bdbbbeeed1949e466e92
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 20:43:42 +0530

    Add files via upload

---
+++ Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array__class
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            // declares an 1D Array of string.
+            string[] topic;
+
+            // allocating memory for topic.
+            topic = new string[] {"Array, ", "String, ",
+                              "Stack, ", "Queue, ",
+                              "Exception, ", "Operators"};
+
+            // Displaying Elements of the array
+            Console.WriteLine("Topic of C#:");
+            Console.WriteLine();
+
+            // Here we calculate and print 
+            // the length of the array, i.e. 6
+            Console.WriteLine("Length of the array: {0}",
+                                           topic.Length);
+            foreach (string ele in topic)
+                Console.WriteLine(ele + " ");
+
+        }
+    }
+}
