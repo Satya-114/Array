7d64969e517e2d6e9d984cf164aae943a27d709b
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 20:37:46 +0530

    Add files via upload

---
+++ Param/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Params_array
+{
+    class Program
+    {
+        static int add(params int[] allnumber)
+        {
+            int sum = 0;
+            foreach (int n in allnumber)
+            {
+                sum = sum + n;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int sum;
+            // passing three parameters
+            sum = Program.add(1, 2, 3);
+            Console.WriteLine("Sum of 1,2,3 is:\t{0}", sum);
+
+            // passing five parameters
+            sum = Program.add(3, 5, 2, 6, 2);
+            Console.WriteLine("Sum of 3,5,2,6,2 is:\t{0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
